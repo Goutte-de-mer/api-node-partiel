a3d0473b53a3b2190910269156f74e6064c2a1bd
Marion <marionbailleux07@gmail.com>
2025-07-25 09:58:36 +0200

    setup article model and route file

---
+++ db/models/Article.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
